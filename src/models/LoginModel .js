0b8e4461a49e0db890b1167094fef80b2866aca3
Pedro Fernandes <contato.phfernandes@gmail.com>
2022-02-26 23:37:11 -0300

    - Criar usuário (Hash de senha, e validação de existência para inserção)

--- src/models/LoginModel .js
+++ src/models/LoginModel .js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -20,11 +21,30 @@ class Login {
     if (this.errors.length > 0) {
       return
     }
+
+
+    await this.userExists()
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+
+
+
+
     try {
+
       this.user = await LoginModel.create(this.body)
+
+
     } catch (error) {
       console.log(error)
     }
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email })
+    if (user) this.errors.push('Email ja cadastrado')
   }
 
   valida() {
